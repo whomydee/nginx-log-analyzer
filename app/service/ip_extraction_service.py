fdec0407c05ad77422b8086c51f82f2e13170bb5
Shad Humydee <shad.humydee@infolytx.com>
2023-03-31 01:52:38 +0600

    add ip extraction service

---
+++ app/service/ip_extraction_service.py
@@ -0,0 +1,20 @@
+import re
+from typing import List
+
+from loguru import logger
+
+
+class IpExtractionService:
+    @staticmethod
+    def get_ip_from_single_line_text(single_line_text: str) -> List[str]:
+        regex_pattern_for_ip = re.compile(r'^\b(?:[0-9]{1,3}\.){3}[0-9]{1,3}\b')
+        ip_addresses = regex_pattern_for_ip.findall(single_line_text)
+
+        logger.debug(f"Found IP: {ip_addresses}")
+
+        return ip_addresses
+
+
+
+
+
