e66166d91c49df58caec36068ffd84348377acbe
Shad Humydee <shad.humydee@infolytx.com>
2023-03-31 18:36:38 +0600

    rafactor into a util

--- tests/util/test_info_extraction_util.py
+++ tests/util/test_info_extraction_util.py
@@ -2,10 +2,10 @@ from unittest import TestCase
 
 from parameterized import parameterized
 
-from app.service.info_extraction_service import InfoExtractionService
+from app.util.info_extraction_util import InfoExtractionUtil
 
 
-class TestInfoExtractionService(TestCase):
+class TestInfoExtractionUtil(TestCase):
     @parameterized.expand([
         ("202.74.246.195 - - [06/Mar/2023:06:55:50 +0000] \"GET /avc-backend/patients?count=50 HTTP/1.1\" 502 497 \"https://raf-optimizer-demo.infolytx.net/" "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36" "-",
         ["202.74.246.195"]),
@@ -13,8 +13,7 @@ class TestInfoExtractionService(TestCase):
         ["52.224.243.156"])
     ])
     def test__get_ip_from_single_line_text__given_single_line_log__extract_correct_ips(self, single_line_text, expected_extracted_ips):
-        info_extraction_service = InfoExtractionService()
-        actual_extracted_ips = info_extraction_service.get_ip_from_single_line_text(single_line_text)
+        actual_extracted_ips = InfoExtractionUtil.get_ip_from_single_line_text(single_line_text)
 
         self.assertEqual(actual_extracted_ips, expected_extracted_ips)
 
@@ -28,7 +27,6 @@ class TestInfoExtractionService(TestCase):
     ])
     def test__get_timestamp_from_single_line_text__given_single_line_log__extract_correct_timestamp(self, single_line_text,
                                                                                        expected_extracted_ips):
-        info_extraction_service = InfoExtractionService()
-        actual_extracted_ips = info_extraction_service.get_timestamp_from_single_line_text(single_line_text)
+        actual_extracted_ips = InfoExtractionUtil.get_timestamp_from_single_line_text(single_line_text)
 
         self.assertEqual(actual_extracted_ips, expected_extracted_ips)
